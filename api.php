5168bbac82662a40eb23577ab9d3becc2992d616
jaeheng <jaeheng@126.com>
2020-12-02 16:42:25 +0800

    remove jquery

--- api.php
+++ api.php
@@ -1,36 +1,48 @@
 <?php
-
-$route = $_GET['route'];
-
+$route = addslashes(trim($_GET['route']));
+// Top10 list
 $topListFilePath = './toplist.json';
+// article json filename
+$articleID = '1';
+// article json data
+$articleJsonFile = "./article/$articleID.json";
+
 // 读文件
 $file = file_get_contents($topListFilePath);
 $data = json_decode($file);
 
 if ($route === 'saveScore') {
-	$post = $_POST;
+	$post = file_get_contents('php://input');
+	$post = json_decode($post, true);
 
-	$name = addslashes(trim($post['name']));
-	$score = intval($post['score']);
+    $name = addslashes(trim($post['name']));
+    $score = intval($post['score']);
 
-	// 插入数据
-	$data[] = [ 'name' => $name, 'score' => $score ];
+    // 插入数据
+    $data[] = ['name' => $name, 'score' => $score];
 
-	// 写文件
-	$res = file_put_contents($topListFilePath, json_encode($data), LOCK_EX);
+    // 写文件
+    $res = file_put_contents($topListFilePath, json_encode($data), LOCK_EX);
 
-	if ($res) {
-		echo json_encode([
-			'state' => 1,
-			'msg' => '成绩已经保存'
-		]);
-	} else {
-		echo json_encode([
-			'state' => 0,
-			'msg' => '保存失败，请重试'
-		]);
-	}
+    if ($res) {
+        echo json_encode([
+            'state' => 1,
+            'msg' => '成绩已经保存',
+        ]);
+    } else {
+        echo json_encode([
+            'state' => 0,
+            'msg' => '保存失败，请重试',
+        ]);
+    }
 }
 
-if ($route === 'toplist') {
+/**
+ * 获取文章数据
+ */
+if ($route === 'article') {
+    $file = file_get_contents($articleJsonFile);
+	$data = json_decode($file);
+
+	echo json_encode($data);
 }
---
+++ script/main.js
@@ -0,0 +1,179 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        line: [],
+        active: 0,
+        route: 'index',
+        toplist: [],
+        submiting: false,
+        form: {
+            name: localStorage.getItem('typetest-name')
+        },
+        time: 0,
+        tik: '',
+        error: [],
+        state: 0 // 0 未开始 1 正在打字
+    },
+    methods: {
+        compareWords (item) {
+            this.start()
+            var inputArr = item.input.split('');
+            var paraph = item.paraph.split('');
+            if (item.input.length > item.paraph.length) {
+                return false;
+            }
+            inputArr.map(function (word, index) {
+                if (paraph[index] === word) {
+                    paraph[index] = '<span class="success">' + paraph[index] + '</span>';
+                } else {
+                    paraph[index] = '<span class="error">' + paraph[index] + '</span>';
+                }
+            });
+            item.paraphDisplay = paraph.join('')
+            this.saveScore()
+        },
+        checkLineOk (index) {
+            var line = this.line[index];
+            if (line.input.length !== line.paraph.length) {
+                this.error[index] = '还没有写完本行'
+                return false;
+            }
+            // 1个字符多29个字符
+            if (line.input.length * 30 !== line.paraphDisplay.length) {
+                this.error[index] = '有错字呢还'
+                return false;
+            }
+            return true;
+        },
+        nextLine (index) {
+            index = parseInt(index)
+            var lines = this.line;
+            // 判断本行是否填写完成
+            if (this.checkLineOk(index)) {
+                if (index !== lines.length - 1) {
+                    this.active = index + 1
+                    this.$refs['input-' + (index + 1)][0].focus()
+                }
+            }
+        },
+        getTopList () {
+            var that = this
+            var time = Date.now()
+            axios.get('./toplist.json?time=' + time).then(function (resp) {
+                resp.data.sort(function (a, b) {
+                    return a.score - b.score;
+                })
+                that.toplist = resp.data
+            }).catch(function (err) {
+                that.showToastify(err)
+            })
+        },
+        saveScore () {
+            var form = this.form
+            var that = this
+            // if (!this.checkWordsOk()) {
+            //     return false;
+            // }
+            form.name = form.name || '匿名'
+            form.score = this.time
+            this.submiting = true
+            axios.post('api.php?route=saveScore', form, {}).then(function (resp) {
+                clearInterval(that.tik)
+                that.showToastify(resp.data.msg, true)
+                that.getTopList()
+                that.submiting = false
+                that.route = 'top'
+            }).catch(function (err) {
+                that.showToastify(err)
+                that.submiting = false
+            })
+        },
+        updateTime () {
+            this.time++
+        },
+        checkWordsOk () {
+            // 检查是不是都写对了
+            var lines = this.line
+            var that = this
+            var isOk = true
+            lines.map(function (item, index) {
+                if (!that.checkLineOk(index)) {
+                    isOk = false
+                    console.log('index:' + index + ' is not ok')
+                }
+            });
+            console.log('isOK:', isOk)
+            return isOk;
+        },
+        start () {
+            var that = this
+            var name = this.form.name.trim()
+            if (name === '') {
+                alert('请填写名称')
+                return false;
+            }
+            if (this.state) {
+                return false
+            }
+            this.active = 0;
+            this.state = 1;
+            this.time++;
+            this.tik = setInterval(function () {
+                that.updateTime()
+            }, 1000)
+            this.$refs['input-0'][0].focus();
+            localStorage.setItem('typetest-name', name);
+        },
+        go (type) {
+            this.route = type
+            if (type === 'top') {
+                this.getTopList()
+            }
+        },
+        /**
+         * 显示信息
+         * @param err
+         * @param type false:error true:success
+         */
+        showToastify (err, type) {
+            const color = !type ? '#ff495b' : '#21bc4a'
+            Toastify({
+                text: err,
+                duration: 3000,
+                newWindow: true,
+                close: true,
+                gravity: "top", // `top` or `bottom`
+                position: "center", // `left`, `center` or `right`
+                backgroundColor: color,
+                stopOnFocus: true, // Prevents dismissing of toast on hover
+                onClick: function(){} // Callback after click
+            }).showToast();
+        },
+        /**
+         * 获取文章数据
+         */
+        getArticle () {
+            const that = this
+            NProgress.start()
+            axios.get('api.php?route=article').then(function (resp) {
+                const data = resp.data
+                if (data) {
+                    that.line = data.map(line => {
+                        return {
+                            paraph: line,
+                            paraphDisplay: line,
+                            input: ''
+                        }
+                    })
+                }
+                NProgress.done();
+            }).catch(function (err) {
+                that.showToastify(err)
+                NProgress.done();
+            })
+        }
+    },
+    mounted: function () {
+        this.getArticle()
+    }
+})
